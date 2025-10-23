5c579de8a91b69cbcb74a8bdbaa6be3d14762699
nkululek0 <nkululekovuyo0103@gmail.com>
2025-10-23 12:55:08 +0200

    (Delete): tailwind-redesign-integration -> removes unused files as repo project structure has changed to accomodate tailwind

--- public/scripts/header-navigation.js
+++
@@ -1,8 +0,0 @@
-// script for making the header drop down //
-
-$(document).ready(function() {
-    $(".fa-caret-square-down").on("click", function() {
-        $(".visibilty").toggle();
-    });
-});
-// end of script for making the header drop down //
